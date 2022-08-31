adb0f68e55f211b4e16351ef79e2133fa8425dcf
Mhn01 <109182781+Mhn01@users.noreply.github.com>
2022-09-01 01:00:07 +0300

    Add files via upload

---
+++ TeoremaLuiPiragora.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+double a;
+double b;
+double c;
+
+cout << "Hai sa calculam teroema lui Pitagora!" << endl;
+cout << "Introdu o valoare catetei (nr.1)" << endl;
+cin >> a;
+cout << "Introdu o valoare catetei (nr.2)"<< endl;
+cin >> b;
+cout << "Iar valoarea ipotenuzei este : " << endl;
+c = sqrt(pow( a , 2) + pow( b , 2));
+cout << c;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
