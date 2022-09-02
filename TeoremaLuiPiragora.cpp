e3fb7a9dfbf7fb3cef6ef6a32e2f404cd0c5dcb9
Mhn01 <109182781+Mhn01@users.noreply.github.com>
2022-09-02 23:59:10 +0300

    Update TeoremaLuiPiragora.cpp

--- TeoremaLuiPiragora.cpp
+++ TeoremaLuiPiragora.cpp
@@ -8,35 +8,34 @@ double a;
 double b;
 double c;
 
+
 cout << "Hai sa calculam teroema lui Pitagora!" << endl;
 cout << "Introdu o valoare catetei (nr.1)" << endl;
 cin >> a;
+
+while (a<0)
+{
+    cout << "Valoarea lui a este invalida!" << endl;
+    cout << "**********************************"<< endl;
+     cout << "Introdu o alta valoarea lui a!" << endl;
+    cin >> a;
+}
+
 cout << "Introdu o valoare catetei (nr.2)"<< endl;
 cin >> b;
+
+while (b<0)
+{
+    cout << "Valoarea lui b este invalida!" << endl;
+    cout << "**********************************"<< endl;
+     cout << "Introdu o alta valoarea lui b!" << endl;
+    cin >> b;
+}
+
+
 cout << "Iar valoarea ipotenuzei este : " << endl;
 c = sqrt(pow( a , 2) + pow( b , 2));
 cout << c;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 return 0;
